33dc3389c1410cab06c929002e95bdd840b9e3f2
4NUB1Ss2 <soprasalvarf@gmail.com>
2026-03-14 18:54:13 -0300

    fix: correcting cors for production

--- config/cors.php
+++ config/cors.php
@@ -3,7 +3,10 @@
 return [
     'paths' => ['api/*'],
     'allowed_methods' => ['*', '*'],
-    'allowed_origins' => ['https://etecgames.com.br'],
+    'allowed_origins' => [
+        'https://etecgames.com.br',
+        'https://www.etecgames.com.br',
+    ],
     'allowed_origins_patterns' => [],
     'allowed_headers' => ['*'],
     'exposed_headers' => [],
