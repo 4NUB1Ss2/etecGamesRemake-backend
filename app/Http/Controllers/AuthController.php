d8ee17425cf92f6575005fe94ff7aeaa9055a9e6
4NUB1Ss2 <soprasalvarf@gmail.com>
2026-04-30 14:00:17 -0300

    arrumando cagadas

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -7,6 +7,8 @@ use App\Http\Requests\StoreUserRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Str;
 
 class AuthController extends Controller
 {
@@ -59,4 +61,45 @@ class AuthController extends Controller
     public function me(Request $request) {
         return response()->json($request->user(),200);
     }
+
+    public function googleLogin(Request $request)
+{
+    $request->validate(['token' => 'required|string']);
+
+    $response = Http::get('https://www.googleapis.com/oauth2/v3/userinfo', [
+        'access_token' => $request->token,
+    ]);
+
+    if (!$response->ok()) {
+        return response()->json(['message' => 'Token inválido'], 401);
+    }
+
+    $payload = $response->json();
+
+    $user = User::where('google_id', $payload['sub'])
+                ->orWhere('email', $payload['email'])
+                ->first();
+
+    if ($user) {
+        if (!$user->google_id) {
+            $user->update(['google_id' => $payload['sub']]);
+        }
+    } else {
+        $user = User::create([
+            'name'              => $payload['name'],
+            'email'             => $payload['email'],
+            'username'          => $this->generateUsername($payload['email']),
+            'password'          => Hash::make(Str::random(32)),
+            'google_id'         => $payload['sub'],
+            'email_verified_at' => now(),
+            'role'              => 'user',
+            'school_id'         => 1,
+        ]);
+    }
+
+    return response()->json([
+        'token' => $user->createToken('auth')->plainTextToken,
+        'user'  => $user,
+    ]);
+}
 }
